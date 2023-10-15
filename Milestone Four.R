336465c73a71ffac0f228dbb4fe74be5b31d0dc2
DelgardoH <144714198+DelgardoH@users.noreply.github.com>
2023-10-15 16:36:24 -0400

    Add files via upload

---
+++ Milestone Four.R
@@ -0,0 +1,4 @@
+hist(twitch$`Peak viewers`, breaks = 20, xlim = c(0, 200000), ylim = c(0, 500), xlab = "Number of Peak Viewers", ylab = "Frequency", main = "Peak Viewers on Twitch Streamers")
+hist(twitch2$`Peak viewers`, breaks = 20, xlim = c(0, 200000), ylim = c(0, 500), xlab = "Number of Peak Viewers", ylab = "Frequency", main = "Peak Viewers on Twitch Streamers")
+hist(twitch$`Average viewers`, breaks = 20, xlim = c(0, 30000), ylim = c(0, 500), xlab = "Number of Average Viewers", ylab = "Frequency", main = "Average Viewers on Twitch Streamers")
+hist(twitch2$`Average viewers`, breaks = 20, xlim = c(0, 30000), ylim = c(0, 500), xlab = "Number of Average Viewers", ylab = "Frequency", main = "Average Viewers on Twitch Streamers")
