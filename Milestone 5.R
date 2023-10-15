3ebc230131ad62715a5fcb191f10e77f382813af
DelgardoH <144714198+DelgardoH@users.noreply.github.com>
2023-10-15 17:27:36 -0400

    Add files via upload

---
+++ Milestone 5.R
@@ -0,0 +1,18 @@
+#MileStone Five
+mean(twitch$`Peak viewers`)
+mean(twitch$`Average viewers`)
+median(twitch$`Peak viewers`)
+median(twitch$`Average viewers`)
+sd(twitch$`Peak viewers`)
+sd(twitch$`Average viewers`)
+var(twitch$`Peak viewers`)
+var(twitch$`Average viewers`)
+
+#MEANS
+#For Milestone Five, I dealt with calculating different values between the two columns above as well. The mean of the 'Peak viewers' was 39.065.5 and 4781.04 for 'Average viewers'. Here it is evident that the peak viewers heavily outclasses the average that a streamer might get. This makes complete sense since someone would not expect their peak to be anywhere lower than their average.
+#MEDIANS
+#The median of the 'Peak viewers' was 16,676 and 2425 for 'Average viewers'. Again, I don't really find this one to be surprising. Though some of the data was heavily skewed, it gives some individuals an idea of how many views they would get if they were in the "middle ground" of those who stream. 
+#STANDARD DEVIATIONS 
+#The standard deviation of the 'Peak viewers' was 60,314.31 and 8453.685 for 'Average viewers'. These are closer to the means than I thought, with the standard deviation of Peak viewers being almost 60% bigger than the mean and the standard deviation of average viewers being about 70% bigger than its mean. The standard deviation being bigger can hint at more skewed data with more variation in some of the values. 
+#VARIANCES
+#The variances of the 'Peak viewers' was 3637815712 and71464789 for 'Average viewers'. 
