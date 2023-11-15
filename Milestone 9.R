40d14ca132b4b1b9a9cd2e8cbbc07afded8c8282
DelgardoH <144714198+DelgardoH@users.noreply.github.com>
2023-11-14 22:36:53 -0500

    Add files via upload

---
+++ Milestone 9.R
@@ -0,0 +1,8 @@
+#Hypothesis Testing One 
+#most of the computations were not done in R but by hand(doing the critical value hypothesis testing), and the results are on the webpage
+
+sum(twitch$Followers > 1000000)
+133/100
+
+sum(twitch$`Stream time(minutes)` > 50000)
+868/1000
